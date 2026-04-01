04e792921f3bdac79f6ab18fa0b09ce0a387cbe4
FlorianAumann <florian.aumann@mimicrobotics.com>
2026-04-01 16:47:10 +0200

    Revert "WIP"
    This reverts commit 18515d8f7d030fd5cb8117cfcdacbcb4f190efa7.

--- combined_impedance_controller/src/combined_impedance_controller.cpp
+++ combined_impedance_controller/src/combined_impedance_controller.cpp
@@ -668,12 +668,11 @@ ctrl::Vector6D CombinedImpedanceController::computeCartMotionError(
   const double max_angle = 0.1;
   const double max_distance = 0.1;
   double angle_clamped = std::clamp(angle, -max_angle, max_angle);
+  distance = std::clamp(distance, 0.0, max_distance);
 
   // Scale errors to allowed magnitudes
   rot_axis = rot_axis * angle_clamped;
-  if (distance > max_distance) {
-    error_kdl.p *= (max_distance / distance);
-  }
+  error_kdl.p = error_kdl.p * distance;
 
   // Reassign values
   ctrl::Vector6D error;
