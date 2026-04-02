f87dfa6bb47566e262ddfe10f668688a125215ab
FlorianAumann <florian.aumann@mimicrobotics.com>
2026-04-02 09:59:09 +0200

    Revert "WIP"
    This reverts commit 361d91cad949919e5d43ee3f264bc8ec438e4423.

--- combined_impedance_controller/include/combined_impedance_controller/ur_robot_monitor.h
+++ combined_impedance_controller/include/combined_impedance_controller/ur_robot_monitor.h
@@ -57,7 +57,6 @@ public:
   static const char *toString(ProgramMode mode);
 
   explicit UrRobotMonitor(rclcpp_lifecycle::LifecycleNode::SharedPtr node);
-  ~UrRobotMonitor() override;
 
   void onConfigure() override;
 
